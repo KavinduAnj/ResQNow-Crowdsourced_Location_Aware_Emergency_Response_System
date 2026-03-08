912e1964a798bb71050fe96583b9c6469884b5ab
GPC Thushani <charithathushani2003@gmail.com>
2026-03-08 10:43:49 +0530

    Fix typo in router definition for incidents

--- backend/routes/incidentRoutes.js
+++ backend/routes/incidentRoutes.js
@@ -1,1 +1,11 @@
+// act as a entry point for all requests related to emergencies 
 
+const express = require('express');
+const router = express.Router();
+const incidentController = require('../controllers/incidentController');
+const authMiddleware = require('../middleware/authMiddleware');
+
+routher.get('/', incidentController.getAllIncidents);
+router.post('/', authMiddleware, incidentController.updateIncidentStatus);
+
+module.exports = router;
